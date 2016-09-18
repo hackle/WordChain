5c826384ed9823ac4ed25558a9e31749e7a2593c
Hackle Wayne <hacklew@hotmail.com>
2016-09-19 01:56:13 +1200

    Allows multiple results

--- WordChain/WordChain.fs
+++ WordChain/WordChain.fs
@@ -24,28 +24,39 @@ let processSet set =
     
 let makeChain set fromWord toWord =
     let processedSet = processSet set
-    let mutable processed:string list = []
-    let mutable chain:Option<string list> = None
+    let mutable chain:string list list = []
     
-    let isWordProcessed w = 
-        List.contains w processed
-
     let rec make currentChain f t =
-        let alreadyProcessed = isWordProcessed f
-        if not alreadyProcessed then        
-            processed <- f :: processed
-
-        if f = t then chain <- Some currentChain
-
-        if alreadyProcessed || (Option.isSome chain) then []
-        else          
+        if f = t then 
+            chain <- currentChain :: chain
+            []
+        else
             match processedSet |> List.filter (fun x -> x.Word = f) with
             | [] -> []
             | fromSet::_ ->
                 fromSet.Neighbors
-                |> List.filter (fun w -> 1 = (getDistance fromSet.Word w.Word) && not (isWordProcessed w.Word))
+                |> List.filter (fun w -> 1 = (getDistance fromSet.Word w.Word) && not (List.contains w.Word currentChain))
                 |> List.map (fun w -> { Word = w.Word;
                                         Distance = getDistance fromSet.Word w.Word 
                                         Neighbors = make (w.Word :: currentChain) w.Word t })
     make [ fromWord ] fromWord toWord |> ignore
     chain
+
+let getChainForWords (fromWord:string) (toWord:string) =
+    let set =
+        async {
+            let client = new System.Net.WebClient()
+            let! html = client.AsyncDownloadString(new System.Uri("http://codekata.com/data/wordlist.txt"))
+            return html.Split([| "\r"; "\n" |], System.StringSplitOptions.RemoveEmptyEntries)
+        }
+        |> Async.RunSynchronously
+        |> Seq.filter (fun s -> s.Length = fromWord.Length)
+        |> Seq.filter (fun s -> toWord.Length > (getDistance s toWord) || toWord.Length > (getDistance s fromWord))
+        |> List.ofSeq
+        |> List.map (fun w -> w.ToLower())
+        |> List.distinct
+
+    let chain = makeChain set fromWord toWord
+    match chain with
+    | None -> printfn "No match was found"
+    | Some c -> printfn "The chain is %A" (c |> List.rev)
