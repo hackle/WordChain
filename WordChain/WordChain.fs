ed8c8fa04a06f7dfb89008a36b9241aacc16fd20
Hackle Wayne <hacklew@hotmail.com>
2016-09-19 19:50:04 +1200

    Hopefully even better

--- WordChain/WordChain.fs
+++ WordChain/WordChain.fs
@@ -9,36 +9,42 @@ let getDistance (word1:string) (word2:string) =
                     c1 <> c2)
     |> Seq.length
     
-let processNeighbors set word =
+let processNeighbors withinMind set word =
     set
     |> List.except [ word ]
     |> List.filter (fun w -> 1 = getDistance word w)
+    |> List.sortBy (fun w -> getDistance w withinMind)  //with this word in mind
     |> List.map (fun w -> { Word = w; 
                             Distance = 1; 
                             Neighbors = [] })
 
-let processSet set =
+let processSet withinMind set =
     set 
     |> List.map (fun w -> { Word = w;
                             Distance = 0;  
-                            Neighbors = processNeighbors set w })
+                            Neighbors = processNeighbors withinMind set w })
 
 type ChainState = { Better: bool; Valid: bool }
 let makeChain set fromWord toWord =
-    let processedSet = processSet set
     let mutable bestChain:Option<string list> = None
     let isChainComplete (chain:string list) (finalWord:string) =
         (chain |> List.head) = finalWord
     
     let rec search (chain:string list) (f:string) (t:string) : string list list =
-        printfn "current chain %A f %A t %A" chain f t
+        let bestLength =
+            match bestChain with
+            | None -> 0
+            | Some bc -> List.length bc
+        printfn "current chain %A f %A t %A length is %i best %i" chain f t (chain|>List.length) bestLength
+
         let isComplete = isChainComplete chain t
         let chainState =
             match bestChain with
             | None -> { Better = isComplete; Valid = not isComplete }
             | Some bc -> 
                 let isBetter = isComplete && (List.length chain) < (List.length bc)
-                let isValid = not isComplete && (List.length chain) < (List.length bc)
+                let stillRoomToGrow = (List.length bc) - (List.length chain) > (getDistance f t)
+                let isValid = not isComplete && stillRoomToGrow
                 { Better = isBetter; Valid = isValid }
                         
         if chainState.Better then
@@ -47,24 +53,21 @@ let makeChain set fromWord toWord =
         // not better and invalid
         elif not chainState.Valid then []
         // not better but still valid
-        else                
-            let currentWords = 
-                processedSet 
-                |> List.filter (fun x -> x.Word = f)
-
-            match currentWords with
-            | fromNode::_ ->
-                fromNode.Neighbors
-                |> List.filter (fun w -> not (List.contains w.Word chain))
-                |> List.map (fun w -> search (w.Word :: chain) w.Word t)
-                |> List.filter (fun l -> List.length l > 0) // get rid of empty lists
-                |> List.concat            
-            | [] -> []
+        elif List.contains f set then
+            set
+            |> List.filter (fun w -> (getDistance f w = 1) && not (List.contains w chain))
+            |> List.except [ f ]
+            |> List.sortBy (fun w -> getDistance w t)
+            |> List.map (fun w -> search (w :: chain) w t)
+            |> List.filter (fun l -> List.length l > 0) // get rid of empty lists
+            |> List.concat
+        else []
 
     search [ fromWord ] fromWord toWord |> ignore
     bestChain
 
 let getChainForWords (fromWord:string) (toWord:string) =
+    let regexJustLetters = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z]+$", System.Text.RegularExpressions.RegexOptions.Compiled)
     let set =
         async {
             let client = new System.Net.WebClient()
@@ -72,7 +75,7 @@ let getChainForWords (fromWord:string) (toWord:string) =
             return html.Split([| "\r"; "\n" |], System.StringSplitOptions.RemoveEmptyEntries)
         }
         |> Async.RunSynchronously
-        |> Seq.filter (fun s -> s.Length = fromWord.Length)
+        |> Seq.filter (fun s -> s.Length = fromWord.Length && regexJustLetters.IsMatch s)
         |> Seq.filter (fun s -> toWord.Length > (getDistance s toWord) || toWord.Length > (getDistance s fromWord))
         |> List.ofSeq
         |> List.map (fun w -> w.ToLower())
