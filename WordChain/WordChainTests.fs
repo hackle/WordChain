b2aff363b1507b17e4313d0d5e8f154ad549e2b1
Hackle Wayne <hacklew@hotmail.com>
2016-09-24 17:03:27 +1200

    Fix tests

--- WordChain/WordChainTests.fs
+++ WordChain/WordChainTests.fs
@@ -9,23 +9,27 @@ let chainGetter () = chain
 
 [<Fact>]
 let ``Chain of a word itself is itself`` () =
+    chain <- None
     let chain = makeChain [ "great" ] "great" "great" 255 shouldCancel chainGetter chainSetter
     Assert.Equal<string list>([ "great" ], chain )
     
 [<Fact>]
 let ``works correctly for small set 1`` () =
+    chain <- None
     let set = [ "cat"; "cow"; "pat"; "cot"; "cog"; "dog" ]
     let chain = makeChain set "cat" "dog" 255 shouldCancel chainGetter chainSetter
     Assert.Equal<string list>([ "cat"; "cot"; "cog"; "dog" ], chain)
     
 [<Fact>]
 let ``works correctly for small set 2`` () =
+    chain <- None
     let set = [ "aft"; "fast"; "raft"; "fist"; "rafts"; "fists"; "rants"; "lists"; "chants"; "list"; "chant"; "lisp" ]
     let chain = makeChain set "aft" "rants" 255 shouldCancel chainGetter chainSetter
     Assert.Equal<string list>([ "aft"; "raft"; "rafts"; "rants" ], chain)
 
 [<Fact>]
 let ``works correctly for small set 2, reversed`` () =
+    chain <- None
     let set = [ "aft"; "fast"; "raft"; "fist"; "rafts"; "fists"; "rants"; "lists"; "chants"; "list"; "chant"; "lisp" ]
     let chain = makeChain set "rants" "aft" 255 shouldCancel chainGetter chainSetter
     Assert.Equal<string list>([ "rants"; "rafts"; "raft"; "aft" ], chain)
